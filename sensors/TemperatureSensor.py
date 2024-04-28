44c07083aaf8ae92a799ec7897b87e1f717487b1
mikic202 <mikolaj.chomanski@gmail.com>
2024-04-28 21:22:40 +0200

    Test all of the sensors (#4)
    * add requirements
    
    * rename init file
    
    * add water level sensor
    
    * add Temperature sensor class
    
    * update requirements
    
    * add food level sensor

---
+++ sensors/TemperatureSensor.py
@@ -0,0 +1,16 @@
+from w1thermsensor import W1ThermSensor
+
+TEMPERATURE_OFFSET = -20.0
+
+
+class TemperatureSensor:
+    def __init__(self):
+        self._probe = W1ThermSensor()
+    
+    def get_temperature(self) -> float:
+        return self._probe.get_temperature() + TEMPERATURE_OFFSET
+
+
+if __name__ == "__main__":
+    sensor = TemperatureSensor()
+    print(sensor.get_temperature())
