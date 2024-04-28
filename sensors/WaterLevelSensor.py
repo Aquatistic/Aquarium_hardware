401caa015765003a41126a1f4bae74ec77041e32
mikic202 <mikolaj.chomanski@gmail.com>
2024-04-28 10:46:55 +0200

    add water level sensor

---
+++ sensors/WaterLevelSensor.py
@@ -0,0 +1,21 @@
+from gpiozero import DistanceSensor
+from time import sleep
+
+
+class WaterLevelSensor:
+    def __init__(self, echo_pin: int, trigger_pin: int) -> None:
+        self._distance_sensor = DistanceSensor(echo=echo_pin, trigger=trigger_pin)
+        self._base_level = self._distance_sensor.distance
+        
+    def get_water_level(self) -> float:
+        return -(self._distance_sensor.distance - self._base_level)*100
+    
+    def reset_sensor(self) -> None:
+        self._base_level = self._distance_sensor.distance
+        
+
+if __name__ == "__main__":
+    sensor = WaterLevelSensor(18, 17)
+    while True:
+        print('Distance: ', sensor.get_water_level())
+        sleep(1)
