6dd064d60453219de04bbdb43e43ba67cfb55acb
mikic202 <mikolaj.chomanski@gmail.com>
2024-04-30 08:10:02 +0200

    5 test effectors and create simple clsses for them (#6)
    * init effectors folder
    
    * add LedStrip class and move water level sensor test pins
    
    * add switch module
    
    * add feeder module
    
    * fix requirenments
    
    * fix black requirenment to newer version
    
    * reformated the code
    
    * change sleep to while loop

--- sensors/FoodLevelSensor.py
+++ sensors/FoodLevelSensor.py
@@ -6,14 +6,13 @@ class FoodLevelSensor:
     def __init__(self, sensor_pin: int) -> None:
         self._sensor_input = DigitalInputDevice(sensor_pin)
         # in the future we can override when activated
-    
+
     def senses_food(self) -> bool:
         return not bool(self._sensor_input.value)
-    
+
 
 if __name__ == "__main__":
     sensor = FoodLevelSensor(21)
-    while(True):
+    while True:
         print(sensor.senses_food())
         sleep(1)
-    
