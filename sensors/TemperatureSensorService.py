143425be22fc36dfccc70af8da07f59afabcddc5
mikic202 <mikolaj.chomanski@gmail.com>
2024-05-03 15:50:39 +0200

    add abstract class as second inheritance

--- sensors/TemperatureSensorService.py
+++ sensors/TemperatureSensorService.py
@@ -1,17 +1,17 @@
 from HardwareSensors.TemperatureSensor import TemperatureSensor
+from SensorService import SensorService
 
-from datetime import datetime
+from datetime import datetime;
 
-
-class TemperatureSensorService(TemperatureSensor):
+class TemperatureSensorService(TemperatureSensor, SensorService):
     def __init__(self, danger_value: float, sensor_id: int = None) -> None:
         super().__init__()
         self._sensor_id = sensor_id
         self._danger_value = danger_value
-
+        
     def set_sensor_id(self, sensor_id: int) -> None:
         self._sensor_id = sensor_id
-
+        
     def get_measurement_json(self) -> None:
         temp_reading = self.get_temperature()
         return f"\{userSensor: {self._sensor_id}, alarmStatus: {self._danger_value > temp_reading}, measurementValue: {temp_reading}, measurementTimestamp: {datetime.now()}\}"
