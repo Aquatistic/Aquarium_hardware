69c6fbcc7ec2a8bfeb2b053d7f4a081ade08db5e
mikic202 <mikolaj.chomanski@gmail.com>
2024-05-04 17:49:29 +0200

    Create run method for sensor backend interactor

--- backend_interactors/SensorsBackendInteractors.py
+++ backend_interactors/SensorsBackendInteractors.py
@@ -3,21 +3,19 @@ from typing import List
 from time import time
 
 class SensorsBackendInteractors:
-    def __init__(self, sensors: List[SensorService], backend_destination: str, readings_period: float):
+    def __init__(self, sensors: List[SensorService], backend_destination: str, send_period: float):
         self._sensors = sensors
         self._backend_destination = backend_destination
-        self._readings_period = readings_period
+        self._send_period = send_period
+        self._measurements = []
         
     def send_measurement(self, measurement: str):
-        pass
+        self._measurements.append(measurement)
         
     def run(self):
         while True:
             loop_start = time()
-            measurement_message = "["
-            for sensor in self._sensors:
-                measurement_message += sensor.get_measurement_json() + ", "
-            measurement_message += "]"
-            requests.post(self._backend_destination, json=measurement_message)
+            requests.post(self._backend_destination, json=str(self._measurements))
+            self._measurements = []
             while time() - loop_start < self._readings_period:
                 pass
