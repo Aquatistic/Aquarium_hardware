b428db80478987d4ba3049ec9ef650afa7a38df3
pkutyl <01168894@pw.edu.pl>
2024-05-19 14:39:41 +0200

    Fix wrong PID variables initialization && notebook visualization

--- regulators/PID.py
+++ regulators/PID.py
@@ -4,17 +4,17 @@ import numpy as np
 
 
 class PID:
-    def __init__(self, Kp: float, Ti: float, Td: float) -> None:
+    def __init__(self, Kp: float, Ti: float, Td: float, Tp: float = 0.5) -> None:
         self.Kp = Kp
         self.Ti = Ti
         self.Td = Td
+        self.Tp = Tp
         self.e = [0.0, 0.0, 0.0]
-        self.x1 = 0  # to be removed
-        self.x2 = 0  # to be removed
+        self.x1 = 0
+        self.x2 = 0
         self.u = self.get_current_control_signal()
         self.y = self.get_current_y()
         self.y_zad = self.get_current_setpoint()
-        self.Tp = 0.5  # to be calculated dynamically?
         self.r0, self.r1, self.r2 = self.calculate_pid_variables()
 
     @staticmethod
@@ -30,14 +30,14 @@ class PID:
         alpha2 = 0.535261
         beta1 = 0.012757
         beta2 = 0.010360
-        g1 = np.exp(7.5 * self.u[-5] - 1) / np.exp(7.5 * self.u[-5] + 1)
+        g1 = (np.exp(7.5 * self.u[-5]) - 1) / (np.exp(7.5 * self.u[-5]) + 1)
         x1 = -alpha1 * self.x1 + self.x2 + beta1 * g1
         x2 = -alpha2 * self.x1 + beta2 * g1
         self.x1 = x1
         self.x2 = x2
         return 1.2 * (1 - np.exp(-1.5 * self.x1)).item()
 
-    def calculate_pid_variables(self) -> tuple[float, float, float]:
+    def calculate_pid_variables(self) -> tuple:
         r0 = self.Kp * (1 + (self.Tp / (2 * self.Ti)) + (self.Td / self.Tp))
         r1 = self.Kp * ((self.Tp / (2 * self.Ti)) - 2 * (self.Td / self.Tp) - 1)
         r2 = self.Kp * self.Td / self.Tp
@@ -49,7 +49,7 @@ class PID:
         return 0.3
 
     def calculate_error(self) -> float:
-        return self.y - self.y_zad
+        return self.y_zad - self.y
 
     def calculate_control_signal(self) -> float:
         return (
@@ -61,19 +61,19 @@ class PID:
 
     def set_u(self) -> None:
         current_u = self.calculate_control_signal()
-        new_u_list = self.u.append(current_u)
-        self.u = new_u_list[-5:]
+        self.u.append(current_u)
+        self.u = self.u[-5:]
 
     def set_e(self) -> None:
         current_e = self.calculate_error()
-        new_e_list = self.e.append(current_e)
-        self.e = new_e_list[-3:]
+        self.e.append(current_e)
+        self.e = self.e[-3:]
 
-    def main_loop(self) -> None:
-        self.get_current_y()
-        self.calculate_pid_variables()
+    def main_loop(self) -> float:
+        self.y = self.get_current_y()
         self.set_e()
         self.set_u()
+        return self.y
 
 
 def main():
