6fa4bf29d7b382dc24dc26322556366338ca4cf4
mikic202 <mikolaj.chomanski@gmail.com>
2024-05-25 16:02:14 +0200

    add starting effectors and sensors at the same time

--- effectors/EffectorService.py
+++ effectors/EffectorService.py
@@ -1,11 +1,24 @@
 from abc import ABC, abstractmethod
 from typing import Dict
+from datetime import datetime
 
 
 class EffectorService(ABC):
     @abstractmethod
     def activate_effector(self, input_dict: Dict[str, str]) -> None:
         pass
+    
+    def activate_effector_in_the_future(self, input_dict: Dict[str, str]) -> None:
+        while datetime.datetime(input_dict['controllActivationMoment']) >= datetime.now():
+            pass
+        self.activate_effector(input_dict)
+    
+    def send_controll_to_effector(self, input_dict: Dict[str, str])) -> None:
+        if datetime.datetime(input_dict['controllActivationMoment']) <= datetime.now():
+            self.activate_effector(input_dict)
+            return
+        Thread(target=self.activate_effector_in_the_future, args=(input_dict,)).start()
+        
     
     def set_effector_id(self, effector_id: int) -> None:
         self._effector_id = effector_id
--- main.py
+++ main.py
@@ -2,17 +2,19 @@ from backend_interactors.SensorsBackendInteractors import SensorsBackendInteract
 from sensors.WaterSensorService import WaterSensorService
 from effectors.SwitchModuleService import SwitchModuleService
 from backend_interactors.EffectorBackendInteractor import EffectorBackendInteractors
+from threading import Thread
 
 
 if __name__ == "__main__":
-    # sensors = [WaterSensorService(23, 22, 1)]
-    # backend = SensorsBackendInteractors(
-    #     "http://192.168.0.100:6868/api/v1/measurements/add", 10.0
-    # )
-    # for sensor in sensors:
-    #     sensor.set_backend_interactor(backend)
-    #     sensor.run()
-    # backend.run()
+    sensors = [WaterSensorService(23, 22, 1)]
+    backend = SensorsBackendInteractors(
+        "http://192.168.0.100:6868/api/v1/measurements/add", 10.0
+    )
+    for sensor in sensors:
+        sensor.set_backend_interactor(backend)
+        sensor.run()
+    backend_thread = Thread(target=self.backend.run)
+    backend_thread.start()
     effectors = [SwitchModuleService(26, 1)]
     backend = EffectorBackendInteractors("http://192.168.0.190:6868/api/v1/userEffector/connect/1", effectors)
     backend.run()
